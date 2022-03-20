d2613c6464d75c1b9eccdffc54f9bf4f0a731d39
mambax7 <mambax7@gmail.com>
2022-03-19 23:36:24 -0400

    formatting, cosmetics

--- admin/admin_footer.php
+++ admin/admin_footer.php
@@ -22,6 +22,8 @@ use XoopsModules\Publisher;
 /** @var Publisher\Helper $helper */
 $helper = Publisher\Helper::getInstance();
 
+/** @var Helper $helper */
+
 $pathIcon32 = Admin::iconUrl('', '32');
 
 echo "<div class='adminfooter'>\n" . "  <div style='text-align: center;'>\n" . "    <a href='https://xoops.org' rel='external'><img src='{$pathIcon32}/xoopsmicrobutton.gif' alt='XOOPS' title='XOOPS'></a>\n" . "  </div>\n" . '  ' . _AM_MODULEADMIN_ADMIN_FOOTER . "\n" . '</div>';
